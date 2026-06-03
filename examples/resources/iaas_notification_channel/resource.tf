33d43adbb19a15c14917769f0620c51d93d97fa4
Bharat Bala <bbharat95@gmail.com>
2026-06-03 14:48:00 +0530

    docs(notification-channel): example tf + generated docs for iaas_notification_channel

---
+++ examples/resources/iaas_notification_channel/resource.tf
@@ -0,0 +1,42 @@
+# Slack notification channel
+resource "iaas_notification_channel" "slack_ops" {
+  name = "ops-slack"
+  type = "slack"
+  config = {
+    webhook_url = "https://hooks.slack.com/services/T000/B000/XXXXXXXXXXXXXXXX"
+  }
+}
+
+# Discord notification channel
+resource "iaas_notification_channel" "discord_alerts" {
+  name = "alerts-discord"
+  type = "discord"
+  config = {
+    webhook_url = "https://discord.com/api/webhooks/000000000000000000/XXXXXXXXXXXX"
+  }
+}
+
+# Telegram notification channel
+resource "iaas_notification_channel" "telegram_bot" {
+  name = "telegram-alerts"
+  type = "telegram"
+  config = {
+    bot_token = "123456789:ABCDefGhIJKlmNoPQRsTUVwxyz"
+    chat_id   = "-1001234567890"
+  }
+}
+
+# Generic webhook notification channel with optional settings
+resource "iaas_notification_channel" "webhook_custom" {
+  name    = "custom-webhook"
+  type    = "webhook"
+  enabled = true
+  config = {
+    url            = "https://example.com/hooks/alerts"
+    method         = "POST"
+    secret         = "my-hmac-signing-secret"
+    connect_timeout = "10"
+    timeout        = "30"
+    verify_ssl     = "true"
+  }
+}
